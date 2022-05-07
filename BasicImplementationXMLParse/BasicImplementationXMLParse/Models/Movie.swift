26988a5514722de34c477e5f254041c928a35154
rizkym <rizkymashudi7@gmail.com>
2022-05-07 17:40:39 +0700

    Practice XMLParse
    Basic implementation XMLParse

---
+++ BasicImplementationXMLParse/BasicImplementationXMLParse/Models/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  BasicImplementationXMLParse
+//
+//  Created by Rizky Mashudi on 07/05/22.
+//
+
+import Foundation
+
+struct Movie {
+  let id: Int
+  let title: String
+  let overview: String
+  let poster: String
+}
---
+++ BasicImplementationXMLParse/BasicImplementationXMLParse/ViewController/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  BasicImplementationXMLParse
+//
+//  Created by Rizky Mashudi on 07/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  var movieTitle = String()
+  var overview = String()
+  var poster = String()
+  var elementName = String()
+  var id = 0
+  var movies = [Movie]()
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    //Get xml file
+    if let path = Bundle.main.url(forResource: "Movies", withExtension: "xml") {
+      if let parser = XMLParser(contentsOf: path) {
+        parser.delegate = self
+        parser.parse()
+      }
+    }
+  }
+
+}
+
+extension ViewController: XMLParserDelegate {
+  func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+    if elementName == "movie" {
+      movieTitle = String()
+      overview = String()
+      poster = String()
+      
+      if let id = attributeDict["id"], let intId = Int(id) {
+        self.id = intId
+      }
+    }
+    
+    self.elementName = elementName
+  }
+  
+  
+  func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+    if elementName == "movie" {
+      let movie = Movie(id: id, title: movieTitle, overview: overview, poster: poster)
+      movies.append(movie)
+    }
+  }
+  
+  func parser(_ parser: XMLParser, foundCharacters string: String) {
+    let data = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    
+    if !data.isEmpty {
+      switch elementName {
+      case "title": movieTitle = data
+      case "overview": overview = data
+      case "poster": poster = data
+      default: break
+      }
+    }
+  }
+  
+  func parserDidEndDocument(_ parser: XMLParser) {
+    movies.forEach { movie in
+      print("ID: \(movie.id)")
+      print("TITLE: \(movie.title)")
+      print("OVERVIEW: \(movie.overview)")
+      print("POSTER: \(movie.poster)")
+    }
+  }
+}
+
